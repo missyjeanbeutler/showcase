a3e0e05f0e0344844c9ad75540e0bd6b4212cd90
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-07-04 16:47:34 -0600

    palindrome

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 class Palindrome extends Component {
+  constructor() {
+    super()
+    this.state = {
+      userInput: '',
+      palindrome: 'wait for it... '
+    }
+  }
+
+  updateUserInput(val) {
+    this.setState({
+      userInput: val
+    })
+  }
+
+  isIt() {
+    let test = this.state.userInput.split('').reverse().join('')
+    this.setState({
+      palindrome: test === this.state.userInput ? 'true' : 'false'
+    })
+  }
+
   render() {
     return (
-      <div>
-        <p>Palindrome</p>
+      <div className="puzzleBox filterStringPB">
+        <h4>Palindrome</h4>
+        <input className="inputLine" onChange={(e) => this.updateUserInput(e.target.value)}/>
+        <button className="confirmationButton" onClick={() => this.isIt()}>Test it!</button>
+        <span className="resultsBox">Is it a palindrome? {this.state.palindrome}</span>
       </div>
     )
   }
